914cc27b66d9f14ee9a8659eecbdd9420d9c5173
bhabani <test@yopmail.com>
2012-12-19 15:06:32 +0530

    Demo application is made

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 DemoApp::Application.routes.draw do
+  resources :micropost1s
+
+
+  resources :tables
+
+
+  resources :microposts
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
