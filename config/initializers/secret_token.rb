9de3faaf728e09ff672c7133c695f62d429f147d
bhabani <test@yopmail.com>
2012-12-19 12:28:34 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b3837e25b6239d721eff7082f414423ff66132aa69870091be38263dee8926af1d369a1d90b74951734ca73d550355481b8a6a7185ce0b959023c1d426456960'
